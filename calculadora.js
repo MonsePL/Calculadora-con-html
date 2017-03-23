0cca8e6d9522fb8019978621a8e989ffcef8f6db
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-03-23 16:59:21 -0600

    calculadoraversion2

--- calculadora.js
+++ calculadora.js
@@ -7,5 +7,19 @@ function agregarNumero(elemento){
 function calculadora(valor){
   var operacion = document.getElementById("caja");
   caja.value = eval(operacion.value);
-
 }
+
+ function teclado(event){
+  var tecladito = event.keycode;
+   if (tecladito == 13){
+     calculadora();
+     return false;
+   } else if (tecladito < 42 || tecladito > 57){
+     return false;
+   }
+ }
+
+ function borrar(numeros){
+   var borrador =document.getElementById("caja")
+   borrador.value = "";
+ }
